86f6420c38b278402300d66d2c8191e19e84447e
rfosator <ramonft@gmail.com>
2023-08-24 22:53:06 +0000

    aritmethic arranger added

---
+++ arithmetic_arranger.py
@@ -0,0 +1,52 @@
+import re
+
+
+class Operation():
+
+  def __init__(self, string: str):
+    regex = r"\+|\-"
+    match = re.search(regex, string)
+    if not match:
+      raise Exception("Error: Operator must be '+' or '-'.")
+    self.operator = match.group()
+
+    values = string.split(self.operator)
+    try:
+      self.val1 = int(values[0])
+      self.val2 = int("{}{}".format(self.operator, values[1].lstrip()))
+    except ValueError:
+      raise Exception("Error: Numbers must only contain digits.")
+
+    self.result = self.val1 + self.val2
+
+  def __str__(self):
+    val1 = str(self.val1).rjust(5, " ")
+    val2 = str(abs(self.val2)).rjust(5, " ")
+    result = str(self.result).rjust(6, " ")
+    return " {}\n{}{}\n------\n{}".format(val1, self.operator, val2, result)
+
+
+def arithmetic_arranger(list: list[str], result: bool = False):
+  if len(list) > 5:
+    raise Exception("Error: Too many problems.")
+
+  op_list = []
+  for item in list:
+    op_list.append(Operation(item))
+
+  first_line = []
+  second_line = []
+  third_line = []
+  fourth_line = []
+  for op in op_list:
+    first_line.append(str(op.val1).rjust(7, " "))
+    second_line.append(" {}{}".format(op.operator,
+                                      str(abs(op.val2)).rjust(5, " ")))
+    third_line.append(" ------")
+    fourth_line.append(str(op.result).rjust(7, " "))
+
+  print(" ".join(first_line))
+  print(" ".join(second_line))
+  print(" ".join(third_line))
+  if result:
+    print(" ".join(fourth_line))
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from arithmetic_arranger import arithmetic_arranger
+
 if __name__ == "__main__":
-  print("Hello World")
+  try:
+    arithmetic_arranger(
+      ["32 + 698", "3801 - 2", "40 - 43", "123 + 49", "5489 - 8632"], True)
+  except Exception as error:
+    print(error)
